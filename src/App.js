eed320dfa5d1e418fb36fa52ac5bf77f7424cbb5
Tim Flood <tflood9@gmail.com>
2021-04-27 15:21:18 -0400

    added calls to brewery api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import TestComponent from "./testComponent";
 
 function App() {
+  const [showComponent, setShowComponent] = useState(true);
   return (
     <div>
-      <TestComponent />
+      {showComponent && <TestComponent />}
+      <button onClick={() => setShowComponent((state) => !state)}>
+        show Test Component
+      </button>
     </div>
   );
 }
--- src/testComponent.jsx
+++ src/testComponent.jsx
@@ -1,13 +1,37 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from "axios"
 
 const TestComponent = () => {
-
+  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
+  const [breweries, setBreweries] = useState([])
+  const [search, setSearch] = useState("")
   useEffect(() => {
-    console.log("useEffect hook ran")
-  })
-  return <div>
-    <h1>TestComponent</h1>
+    const fetchData = async () => {
+      const results = await axios.get(`https://api.openbrewerydb.org/breweries?by_name=${search}`)
+      // setPost(results.data)
+      setBreweries(results.data)
+      console.log(results)
+    };
+    fetchData()
+  },[search]);
+
+
+
+  console.log("component rendered");
+  return (
+    <div>
+      <h1>TestComponent</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(state => state+1)}>Increment</button>
+      <button onClick={() => setDarkMode(state => !state)}>Toggle DarkMode</button>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" />
+
+      {breweries.map((brewery) => {
+        return <h2>{brewery.name}</h2>;
+    })}
   </div>
+  )
 }
 
 export default TestComponent
